c72b628bb7e60f1b329c19ed81a78bb55a387c24
bazzi24 <136590069+bazzi24@users.noreply.github.com>
2026-01-31 11:15:08 -0500

    Add class method

--- api/db/services/db_models.py
+++ api/db/services/db_models.py
@@ -141,3 +141,42 @@ class BaseModel(Model):
     @classmethod
     def getter_by(cls, attr):
         return operator.attrgetter(attr)(cls)
+    
+    @classmethod
+    def query(cls, reverse=None, order_by=None, **kwargs):
+        filter = []
+        for f_n, f_v in kwargs.items():
+            attr_name = "%s" % f_n
+            if not hasattr(cls, attr_name) or f_v is None:
+                continue
+            if type(f_v) in {list, set}:
+                f_v = list(f_v)
+                if is_continuouse_field(type(getattr(cls, attr_name))):
+                    if len(f_v) == 2:
+                        for i, v in enumerate(f_v):
+                            if isinstance(v, str) and f_n in auto_date_timestamp_field():
+                                f_v[i] = date_string_to_timestamp(v)
+                        lt_value = f_v[0]
+                        gt_value = f_v[1]
+                        if lt_value is not None and gt_value is not None:
+                            filter.append(cls.getter_by(attr_name).between(lt_value, gt_value))
+                        elif lt_value is not None:
+                            filter.append(operator.attrgetter(attr_name)(cls) >= lt_value)
+                        elif gt_value is not None:
+                            filter.append(operator.attrgetter(attr_name)(cls) <= gt_value)
+                else:
+                    filter.append(operator.attrgetter(attr_name)(cls) << f_v)
+            else:
+                filter.append(operator.attrgetter(attr_name)(cls) == f_v)
+        if filter:
+            query_records = cls.select().where(*filter)
+            if reverse is not None:
+                if not order_by or not hasattr(cls, f"{order_by}"):
+                    order_by = "create_time"
+                if reverse is True:
+                    query_records = query_records.order_by(cls.getter_by(f"{order_by}").desc())
+                elif reverse is False:
+                    query_records = query_records.order_by(cls.getter_by(f"{order_by}").asc())
+            return [query_records for query_record in query_records]
+        else:
+            return []    
