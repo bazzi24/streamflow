5f2ebd1c18607814a5fe19b0b216f899b01337a5
bazzi <136590069+bazzi24@users.noreply.github.com>
2026-04-16 00:59:11 +0700

    Refactor: Remove the broken dead code (config_utils.py, setting.py) entirely

--- common/setting.py
+++
@@ -1,9 +0,0 @@
-import os 
-import json
-import secrets
-from datetime import date
-import logging
-from common.config_utils import decrypt_database_config
-
-DATABASE_TYPE = os.getenv("DB_TYPE", "mysql")
-DATABASE = decrypt_database_config(name=DATABASE_TYPE)
